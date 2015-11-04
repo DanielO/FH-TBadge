008235e80dbf0db0c87375b7324e21a49f10dcb8
Daniel O'Connor <darius@dons.net.au>
2015-11-04 11:08:23 +1030

    Add Conway's game of life
    Code from http://brownsofa.org/blog/2010/12/30/arduino-8x8-game-of-life/

--- Fh@tBadge.ino
+++ Fh@tBadge.ino
@@ -10,20 +10,20 @@
 #define COL_OFF 	LOW
 #define ROW_ON		LOW				// Rows are Cathodes
 #define ROW_OFF		HIGH
-const int numCol = 8;				// The display has 8 columns
-const int numRow = 8;				// and 8 rows
+#define NUM_COL		8
+#define NUM_ROW		8
 
 // FH@T Badge display with black back
-//const int colPin[numCol] = { 8, 13,7,11,0,6,1,4 };		// Thes are the pins that the columns are connected to in order from col 0
-//const int rowPin[numRow] = { 15,14,10,5,9,3,2,12 };		// Thes are the pins that the rows are connected to in order from row 0
+//const int colPin[NUM_COL] = { 8, 13,7,11,0,6,1,4 };		// Thes are the pins that the columns are connected to in order from col 0
+//const int rowPin[NUM_ROW] = { 15,14,10,5,9,3,2,12 };		// Thes are the pins that the rows are connected to in order from row 0
 
 // FH@T Badge display with white back
-const int colPin[numCol] = { 4, 1,6,0,11,7,13,8 };			// Thes are the pins that the columns are connected to in order from col 0
-const int rowPin[numRow] = { 12,2,3,9,5,10,14,15 };			// Thes are the pins that the rows are connected to in order from row 0
+const int colPin[NUM_COL] = { 4, 1,6,0,11,7,13,8 };			// Thes are the pins that the columns are connected to in order from col 0
+const int rowPin[NUM_ROW] = { 12,2,3,9,5,10,14,15 };			// Thes are the pins that the rows are connected to in order from row 0
 
 // Tony's display
-//const int colPin[numCol] = { 0, 1,2,3,4,5,6,7 };			// Thes are the pins that the columns are connected to in order from col 0
-//const int rowPin[numRow] = { 8,9,10,11,12,22,23,24 };		// Thes are the pins that the rows are connected to in order from row 0
+//const int colPin[NUM_COL] = { 0, 1,2,3,4,5,6,7 };			// Thes are the pins that the columns are connected to in order from col 0
+//const int rowPin[NUM_ROW] = { 8,9,10,11,12,22,23,24 };		// Thes are the pins that the rows are connected to in order from row 0
 
 // Display configuration
 #define MIRROR_ROW
@@ -90,6 +90,18 @@ static const uint8_t PROGMEM splash_T[] = { 0x7E, 0x7E, 0x18, 0x18, 0x18, 0x18, 
 
 #include "font.h"
 
+// Conway Life boards
+static const uint8_t board1[NUM_ROW][NUM_COL] = {
+	{ 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 1, 1, 0, 0, 0, 0 },
+	{ 0, 1, 1, 0, 0, 0, 0, 0 },
+	{ 0, 0, 1, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0 }
+};
+
 /**********************************************************************
  ************************* HELPER ROUTINES ****************************
  **********************************************************************/
@@ -98,7 +110,7 @@ static const uint8_t PROGMEM splash_T[] = { 0x7E, 0x7E, 0x18, 0x18, 0x18, 0x18, 
 static void __attribute__((interrupt)) myISR() {
 	RefreshDisplay();
 
-	colIdx = (colIdx + 1) % numCol;
+	colIdx = (colIdx + 1) % NUM_COL;
 
 	clearIntFlag(_TIMER_3_IRQ);
 }
@@ -122,11 +134,11 @@ static void RefreshDisplay() {
 	int c, r, rowPtr, tmp;
 
 	// De-select old column
-	for (int c = 0; c < numCol; c++)
+	for (int c = 0; c < NUM_COL; c++)
 		digitalWrite(colPin[c], COL_OFF);
 
 	// De-select old rows
-	for (int r = 0; r < numRow; r++)
+	for (int r = 0; r < NUM_ROW; r++)
 		digitalWrite(rowPin[r], ROW_OFF);
 
 	// Select current column
@@ -140,7 +152,7 @@ static void RefreshDisplay() {
 	c = 7 - c;
 #endif
 	// Light up each row as necessary
-	for (rowPtr = 0; rowPtr < numRow; rowPtr++) {
+	for (rowPtr = 0; rowPtr < NUM_ROW; rowPtr++) {
 		r = rowPtr;
 #ifdef MIRROR_ROW
 		r = 7 - r;
@@ -185,14 +197,14 @@ static const int getCharLen(char c) {
  **********************************************************************/
 // clears the display, All LEDs off
 void ClearDisplay(void) {
-	for (int i = 0; i < numCol; i++)
+	for (int i = 0; i < NUM_COL; i++)
 		display[i] = 0;
 }
 
 // Dots all on
 void FillDisplay(void)
 {
-	for (int i = 0; i < numCol; i++)
+	for (int i = 0; i < NUM_COL; i++)
 		display[i] = 0xff;
 }
 
@@ -204,8 +216,8 @@ void DisplayChar(char c) {
 }
 
 // Displays an 8 x 8 splash screen
-void DisplaySplash(const uint8_t splash[numCol]) {
-	for (int i = 0; i < numCol; i++)
+void DisplaySplash(const uint8_t splash[NUM_COL]) {
+	for (int i = 0; i < NUM_COL; i++)
 		display[i] = splash[i];
 }
 
@@ -213,9 +225,9 @@ void DisplaySplash(const uint8_t splash[numCol]) {
 // if state is 1 LED will be turned on
 // if state is 0 LED will be off
 void DrawPixel( int x, int y, int state) {
-	if ((y < 0) || (y >= numCol))
+	if ((y < 0) || (y >= NUM_COL))
 		return;
-	if ((x < 0) || (x >= numRow))
+	if ((x < 0) || (x >= NUM_ROW))
 		return;
 
 	if (state)
@@ -258,12 +270,12 @@ void DisplayText(const char PROGMEM *msg, int coldelay) {
 		}
 
 		// Scroll left everything in the display
-		for (int r = 0; r < numRow; r++) {
+		for (int r = 0; r < NUM_ROW; r++) {
 			display[r] = display[r] << 1;
 		}
 
 		// Add new data in
-		for (int r = 0; r < numRow; r++) {
+		for (int r = 0; r < NUM_ROW; r++) {
 			display[r] |= (fdata[r] >> charpos) & 0x01;
 		}
 
@@ -287,16 +299,110 @@ void DisplayAnim(const uint8_t PROGMEM *anim, const uint8_t PROGMEM *durs, int n
 	}
 }
 
+// Run Conway's game of life
+// http://brownsofa.org/blog/2010/12/30/arduino-8x8-game-of-life/
+/**
+ * Counts the number of active cells surrounding the specified cell.
+ * Cells outside the board are considered "off"
+ * Returns a number in the range of 0 <= n < 9
+ */
+uint8_t countNeighbors(uint8_t board[NUM_ROW][NUM_COL], uint8_t row, uint8_t col) {
+	uint8_t count = 0;
+	for (char rowDelta = -1; rowDelta <= 1; rowDelta++) {
+		for (char colDelta = -1; colDelta <= 1; colDelta++) {
+			// skip the center cell
+			if (!(colDelta == 0 && rowDelta == 0)) {
+				if (isCellAlive(board, rowDelta + row, colDelta + col)) {
+					count++;
+				}
+			}
+		}
+	}
+	return count;
+}
+
+/**
+ * Returns whether or not the specified cell is on.
+ * If the cell specified is outside the game board, returns false.
+ */
+boolean isCellAlive(uint8_t board[NUM_ROW][NUM_COL], char row, char col) {
+	if (row < 0 || col < 0 || row >= NUM_ROW || col >= NUM_COL) {
+		return false;
+	}
+
+	return (board[row][col] == 1);
+}
+
+/**
+ * Encodes the core rules of Conway's Game Of Life, and generates the next iteration of the board.
+ * Rules taken from wikipedia.
+ */
+void calculateNewGameBoard(uint8_t oldboard[NUM_ROW][NUM_COL], uint8_t newboard[NUM_ROW][NUM_COL]) {
+	for (uint8_t row = 0; row < NUM_ROW; row++) {
+		for (uint8_t col = 0; col < NUM_COL; col++) {
+			uint8_t numNeighbors = countNeighbors(oldboard, row, col);
+
+			if (oldboard[row][col] && numNeighbors < 2) {
+				// Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+				newboard[row][col] = false;
+			} else if (oldboard[row][col] && (numNeighbors == 2 || numNeighbors == 3)) {
+				// Any live cell with two or three live neighbours lives on to the next generation.
+				newboard[row][col] = true;
+			} else if (oldboard[row][col] && numNeighbors > 3) {
+				// Any live cell with more than three live neighbours dies, as if by overcrowding.
+				newboard[row][col] = false;
+			} else if (!oldboard[row][col] && numNeighbors == 3) {
+				// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+				newboard[row][col] = true;
+			} else {
+				// All other cells will remain off
+				newboard[row][col] = false;
+			}
+		}
+	}
+}
+
+// Copy newboard to oldboard
+void swapGameBoards(uint8_t oldboard[NUM_ROW][NUM_COL], uint8_t newboard[NUM_ROW][NUM_COL]) {
+	for (uint8_t row = 0; row < NUM_ROW; row++) {
+		for (uint8_t col = 0; col < NUM_COL; col++) {
+			oldboard[row][col] = newboard[row][col];
+		}
+	}
+}
+
+void DisplayConway(const uint8_t PROGMEM start[NUM_ROW][NUM_COL], int time, int dtime) {
+	uint8_t oldboard[NUM_ROW][NUM_COL] = { 0 };
+	uint8_t newboard[NUM_ROW][NUM_COL] = { 0 };
+
+	memcpy(oldboard, start, sizeof(oldboard));
+
+	for (int t = 0; t < time * 1000 / dtime; t++) {
+		for (uint8_t row = 0; row < NUM_ROW; row++) {
+			uint8_t tmp = 0;
+			for (uint8_t col = 0; col < NUM_COL; col++) {
+				if (oldboard[row][col])
+					tmp |= 1 << col;
+			}
+			display[row] = tmp;
+		}
+		delay(dtime);
+		calculateNewGameBoard(oldboard, newboard);
+		swapGameBoards(oldboard, newboard);
+	}
+
+}
+
 /* Arduino setup function, called once */
 void setup() {
 	invertDisplay = false;
 	// Set all column pins to outputs and off
-	for (int i = 0; i < numCol; i++) {
+	for (int i = 0; i < NUM_COL; i++) {
 		pinMode(colPin[i], OUTPUT);
 		digitalWrite(colPin[i], COL_OFF);
 	}
 	// Set all row pins to outputs and off
-	for (int i = 0; i < numCol; i++) {
+	for (int i = 0; i < NUM_COL; i++) {
 		pinMode(rowPin[i], OUTPUT);
 		digitalWrite(rowPin[i], ROW_OFF);
 	}
@@ -333,6 +439,11 @@ void loop() {
 	ClearDisplay();
 	delay(500);
 	DisplayText(msg, 75);
+
+	// Display Conway's game of life for 8 seconds at 8 FPS
+	delay(500);
+	ClearDisplay();
+	DisplayConway(board1, 8, 125);
 
 	// Display Matrix for 10 seconds at 8 FPS
 	ClearDisplay();
